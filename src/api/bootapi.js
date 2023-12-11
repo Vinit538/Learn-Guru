bd0244065a4ee7a009dd8eae7ac6eac73e571a51
Vinit538 <vinitkaratagi538@gmail.com>
2023-12-11 12:41:59 +0530

    api_update1

--- src/api/bootapi.js
+++ src/api/bootapi.js
@@ -1,5 +1,5 @@
 // const base_url="http://localhost:8095";
 // export default base_url;
 
-const base_url = "http://192.168.164.145:8095";
+const base_url = "https://192.168.164.145:8095";
 export default base_url;
